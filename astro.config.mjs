1e65e574c4e0f8154847fedc3f58630f1744d5b5
Khushit Shah <ping@khushitshah.com>
2026-07-10 00:36:45 +0530

    Support GitHub user pages site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,16 @@ import tailwind from "@astrojs/tailwind";
 const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
 const owner = process.env.GITHUB_REPOSITORY_OWNER;
 const isGithubPages = process.env.GITHUB_PAGES === "true";
+const isUserPagesRepo = repo === `${owner}.github.io`;
 
 export default defineConfig({
   site: process.env.SITE_URL ?? (
     isGithubPages && repo && owner
-      ? `https://${owner}.github.io/${repo}`
+      ? (isUserPagesRepo ? `https://${owner}.github.io` : `https://${owner}.github.io/${repo}`)
       : "https://astro-nano-demo.vercel.app"
   ),
   base: process.env.BASE_PATH ?? (
-    isGithubPages && repo
+    isGithubPages && repo && !isUserPagesRepo
       ? `/${repo}`
       : "/"
   ),
